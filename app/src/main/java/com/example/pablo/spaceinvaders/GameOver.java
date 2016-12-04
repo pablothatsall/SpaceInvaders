a61da89dd2bcbce09b93fc4228995f6b61c4a236
Pablo <pablothatsall@gmail.com>
2016-12-05 00:32:19 +0100

    poi

--- app/src/main/java/com/example/pablo/spaceinvaders/GameOver.java
+++ app/src/main/java/com/example/pablo/spaceinvaders/GameOver.java
@@ -14,11 +14,13 @@ import static com.example.pablo.spaceinvaders.R.layout.gameover;
 
 public class GameOver extends Activity {
     Button jugarDeNuevo;
+    Button volverMenu;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(gameover);
         jugarDeNuevo = (Button) findViewById(R.id.button3);
+        volverMenu = (Button) findViewById(R.id.button6);
 
         jugarDeNuevo.setOnClickListener(new View.OnClickListener() {
 
@@ -26,9 +28,20 @@ public class GameOver extends Activity {
             public void onClick(View view) {
                 // TODO Auto-generated method stub
                 Intent gameoverintent = new Intent(GameOver.this, SpaceInvadersActivity.class);
+                finish();
                 startActivity(gameoverintent);
             }
         });
 
+        volverMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO Auto-generated method stub
+                Intent menuintent = new Intent(GameOver.this, Inicio.class);
+                finish();
+                startActivity(menuintent);
+            }
+        });
     }
+
 }
